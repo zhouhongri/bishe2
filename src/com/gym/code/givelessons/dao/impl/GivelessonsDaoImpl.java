dae7c414db0d4f85c4eb29092d25e2e98855f4fd
zhr <1305709327@qq.com>
2018-06-21 09:00:46 +0800

    coach0621

--- src/com/gym/code/givelessons/dao/impl/GivelessonsDaoImpl.java
+++ src/com/gym/code/givelessons/dao/impl/GivelessonsDaoImpl.java
@@ -1,7 +1,15 @@
 package com.gym.code.givelessons.dao.impl;
 
+import javax.servlet.http.HttpServletRequest;
+
 import com.gym.code.givelessons.dao.GivelessonsDaoInter;
 
 public class GivelessonsDaoImpl implements GivelessonsDaoInter {
+
+	@Override
+	public void queryGivelessonsDao(HttpServletRequest req, int currpage) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
